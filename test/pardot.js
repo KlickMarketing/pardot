3d68a654269261723880bd4f0266145b1999b260
iDVB <me@danvanbrunt.com>
2019-09-30 13:44:56 -0400

    fix(deps): Update all deps

--- test/pardot.js
+++ test/pardot.js
@@ -5,92 +5,84 @@ var lab     = exports.lab = Lab.script();
 
 var pardot = require('../lib/pardot');
 
+lab.test('authenticate fails when userKey is not specified',  async () => {
 
-lab.test('authenticate fails when userKey is not specified', function(done) {
-
-  pardot({
+  await pardot({
     email: 'test@gmail.com',
     password: 'easy'
-  }).fail(function(err) {
-    expect(err.message).to.equal('userKey must be specified');
-    done();
-  }).done();
+  }).fail(function (err) {
+    expect(err.toString()).to.contain('userKey must be specified');
+  });
 
 });
 
-lab.test('authenticate fails when userKey is blank', function(done) {
+lab.test('authenticate fails when userKey is blank', async () => {
 
   pardot({
     userKey: '',
     email: 'test@gmail.com',
     password: 'easy'
   }).fail(function(err) {
-    expect(err.message).to.equal('userKey must be specified');
-    done();
-  }).done();
+    expect(err.toString()).to.contain('userKey must be specified');
+  });
 
 });
 
-lab.test('authenticate fails when email is not specified', function(done) {
+lab.test('authenticate fails when email is not specified', async () => {
 
   pardot({
     userKey: 'abc123',
     password: 'easy'
-  }).fail(function(err) {
-    expect(err.message).to.equal('email must be specified');
-    done();
-  }).done();
+  }).fail(function (err) {
+    expect(err.toString()).to.contain('email must be specified');
+  });
 
 });
 
-lab.test('authenticate fails when email is blank', function(done) {
+lab.test('authenticate fails when email is blank', async () => {
 
   pardot({
     userKey: 'abc123',
     email: '',
     password: 'easy'
   }).fail(function(err) {
-    expect(err.message).to.equal('email must be specified');
-    done();
-  }).done();
+    expect(err.toString()).to.contain('email must be specified');
+  });
 
 });
 
-lab.test('authenticate fails when password is not specified', function(done) {
+lab.test('authenticate fails when password is not specified', async () => {
 
   pardot({
     userKey: 'abc123',
     email: 'test@gmail.com',
-  }).fail(function(err) {
-    expect(err.message).to.equal('password must be specified');
-    done();
-  }).done();
+  }).fail(function (err) {
+    expect(err.toString()).to.contain('password must be specified');
+  });
 
 });
 
-lab.test('authenticate fails when password is not specified', function(done) {
+lab.test('authenticate fails when password is not specified', async () => {
 
   pardot({
     userKey: 'abc123',
     email: 'test@gmail.com'
-  }).fail(function(err) {
-    expect(err.message).to.equal('password must be specified');
-    done();
-  }).done();
+  }).fail(function (err) {
+    expect(err.toString()).to.contain('password must be specified');
+  });
 
 });
 
 
-lab.test('authenticate fails when password is blank', function(done) {
+lab.test('authenticate fails when password is blank', async () => {
 
   pardot({
     userKey: 'abc123',
     email: 'test@gmail.com',
     password: ''
-  }).fail(function(err) {
-    expect(err.message).to.equal('password must be specified');
-    done();
-  }).done();
+  }).fail(function (err) {
+    expect(err.toString()).to.contain('password must be specified');
+  });
 
 });
 
